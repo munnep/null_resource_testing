6ace909b2d5c406946244cf75e7fd07822297142
munnep <patrick.munne@hashicorp.com>
2024-07-09 09:52:30 +0200

    test 445

--- main.tf
+++ main.tf
@@ -5,3 +5,33 @@ resource "null_resource" "test2" {}
 resource "null_resource" "test3" {}
 
 resource "null_resource" "test44" {}
+
+
+resource "null_resource" "test445" {}
+
+
+
+
+
+
+
+# variable "pet_words" {
+#   default     = 10
+#   description = "Number of words per pet"
+# }
+# variable "pets_count" {
+#   default     = 2500
+#   description = "Count of pets"
+# }
+# variable "string_length" {
+#   default = 512
+# }
+# 
+# resource "random_pet" "ourhoard" {
+#   count  = var.pets_count
+#   length = var.pet_words
+#   keepers = {
+#     # Generate a new pet name each time the time changes
+#     make_on_every_apply = timestamp()
+#   }
+# }
