56f9d88b14b2d286aa4bc31306212cf2b190aef2
munnep <patrick.munne@hashicorp.com>
2025-02-20 10:32:19 +0100

    change to 119s

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 resource "null_resource" "previous" {}
 
-resource "time_sleep" "wait_120_seconds" {
+resource "time_sleep" "wait_119_seconds" {
   depends_on = [null_resource.previous]
 
-  create_duration = "120s"
+  create_duration = "119s"
 }
 
 # This resource will create (at least) 30 seconds after null_resource.previous
 resource "null_resource" "next" {
-  depends_on = [time_sleep.wait_120_seconds]
+  depends_on = [time_sleep.wait_119_seconds]
 }
