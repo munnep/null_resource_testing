681d5ff26628e2d049ea4e3620a04a4c441ba515
munnep <patrick.munne@hashicorp.com>
2025-06-18 11:45:39 +0200

    shell1

--- main.tf
+++ main.tf
@@ -1,54 +1,26 @@
 terraform {
   required_providers {
-    acme = {
-      source = "myklst/acme"
-      version = "0.2.0"
-    }
-        gitlab = {
-      source = "gitlabhq/gitlab"
-      version = "18.0.0"
-    }
-        postgresql = {
-      source = "cyrilgdn/postgresql"
-      version = "1.25.0"
-    }
-        mysql = {
-      source = "petoju/mysql"
-      version = "3.0.76"
-    }
-        biganimal = {
-      source = "EnterpriseDB/biganimal"
-      version = "2.0.0"
-    }
-    spotify = {
-      source = "conradludgate/spotify"
-      version = "0.2.7"
+    shell = {
+      source = "scottwinkler/shell"
+      version = "1.7.10"
     }
   }
 }
 
-resource "null_resource" "test5" {}
-
-provider "spotify" {
+provider "shell" {
   # Configuration options
 }
 
-provider "biganimal" {
-  # Configuration options
-}
+resource "shell_script" "github_repository" {
+  lifecycle_commands {
+    create = file("${path.module}/create.sh")
+    read   = file("${path.module}/read.sh")
+    update = file("${path.module}/update.sh")
+    delete = file("${path.module}/delete.sh")
+  }
 
-provider "acme" {
-  # Configuration options
-}
-
-provider "mysql" {
-  # Configuration options
-}
-
-provider "gitlab" {
-  # Configuration options
-}
-
-provider "postgresql" {
-  # Configuration options
+  environment = {
+    NAME        = "HELLO-WORLD"
+    DESCRIPTION = "description"
+  }
 }
