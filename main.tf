b64f6a0901dc9a139d4826ac741509e3c2639652
munnep <patrick.munne@hashicorp.com>
2025-06-18 10:28:56 +0200

    providers

--- main.tf
+++ main.tf
@@ -1,12 +1,52 @@
-resource "null_resource" "previous" {}
-
-resource "time_sleep" "wait_119_seconds" {
-  depends_on = [null_resource.previous]
-
-  create_duration = "119s"
+terraform {
+  required_providers {
+    acme = {
+      source = "myklst/acme"
+      version = "0.2.0"
+    }
+        gitlab = {
+      source = "gitlabhq/gitlab"
+      version = "18.0.0"
+    }
+        postgresql = {
+      source = "cyrilgdn/postgresql"
+      version = "1.25.0"
+    }
+        mysql = {
+      source = "petoju/mysql"
+      version = "3.0.76"
+    }
+        biganimal = {
+      source = "EnterpriseDB/biganimal"
+      version = "2.0.0"
+    }
+    spotify = {
+      source = "conradludgate/spotify"
+      version = "0.2.7"
+    }
+  }
 }
 
-# This resource will create (at least) 30 seconds after null_resource.previous
-resource "null_resource" "next" {
-  depends_on = [time_sleep.wait_119_seconds]
+provider "spotify" {
+  # Configuration options
+}
+
+provider "biganimal" {
+  # Configuration options
+}
+
+provider "acme" {
+  # Configuration options
+}
+
+provider "mysql" {
+  # Configuration options
+}
+
+provider "gitlab" {
+  # Configuration options
+}
+
+provider "postgresql" {
+  # Configuration options
 }
