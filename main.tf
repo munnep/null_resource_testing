f537ea75f5ea3f92844cad3f5748f946ea7b0947
munnep <patrick.munne@hashicorp.com>
2025-09-26 12:01:41 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 # This resource will destroy (potentially immediately) after null_resource.next
-resource "null_resource3" "previous" {}
+resource "null_resource4" "previous" {}
 
-resource "time_sleep3" "wait_300_seconds" {
-  depends_on = [null_resource.previous]
+resource "time_sleep4" "wait_300_seconds" {
+  depends_on = [null_resource4.previous]
 
   create_duration = "300s"
 }
 
 # This resource will create (at least) 300 seconds after null_resource.previous
-resource "null_resource3" "next" {
-  depends_on = [time_sleep3.wait_300_seconds]
+resource "null_resource4" "next" {
+  depends_on = [time_sleep4.wait_300_seconds]
 }
